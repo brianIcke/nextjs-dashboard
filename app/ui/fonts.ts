c312fdc30169c00c413a81cb53fc4fc36a90ee70
Brian Nickel <mail@brian-nickel.de>
2024-04-20 03:27:27 +0200

    page.tsx: added hero image acme-logo.tsx -> swbs-logo.tsx: changed style fonts.ts: added fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Fugaz_One } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'] ,weight: '400' });
+export const fugaz_one = Fugaz_One({ subsets: ['latin'] ,weight: '400' });
---
+++ app/ui/swbs-logo.tsx
@@ -0,0 +1,13 @@
+import { Cog8ToothIcon } from '@heroicons/react/24/outline';
+import { fugaz_one } from '@/app/ui/fonts';
+
+export default function SWBSLogo() {
+  return (
+    <div
+      className={`${fugaz_one.className} flex flex-row items-center leading-none text-white`}
+    >
+      <Cog8ToothIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">SWBS</p>
+    </div>
+  );
+}
